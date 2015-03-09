87e02e0068b97cca99457c40d0f7420b19737443
ben <ben.spoor@gmail.com>
2015-03-09 23:43:24 +0100

    Updated docs

---
+++ doc/generate_ghpages.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/bash
+
+current_dir=${PWD##*/}
+
+if [ "${current_dir}" == "doc" ]; then
+    cd ..
+    current_dir=${PWD##*/}
+fi
+
+if [ "${current_dir}" == "remedi" ]; then
+    mkdir -p bld
+    cd bld
+    cmake ..
+    make doc
+
+    cp -R ./doc/html/* ../doc/html
+
+    git add -A
+    git commit -m "Updated docs"
+else
+    echo "Must be in project root"
+fi
